916849ebc1a8d0192af83200b1fffe1c3c611e62
likejehu <etqash@gmail.com>
2020-02-12 20:57:24 +0200

    changes to main

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
+type server struct{}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"message": "welcome to library"}`))
+}
+
 func main() {
-	http.HandleFunc("/library", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Игорь, добро пожаловать в библиотеку")
-	})
-	http.ListenAndServe(":8080", nil)
+	s := &server{}
+	http.Handle("/library", s)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
