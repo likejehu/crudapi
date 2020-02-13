c152583177c42a1d8518425104cb876769372d55
likejehu <etqash@gmail.com>
2020-02-13 17:22:45 +0200

    added mux handling to main

--- main.go
+++ main.go
@@ -7,31 +7,37 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func road(w http.ResponseWriter, r *http.Request) {
+func read(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("get(read) called"))
+}
 
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("get(read) called"))
-	case "POST":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("post(create) called"))
-	case "PUT":
-		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte("put(update) called"))
-	case "DELETE":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("delete called"))
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("not found"))
-	}
+func create(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("post(create) called"))
+}
 
+func update(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("put(update) called"))
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("delete called"))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("not found"))
 }
 
 func main() {
 	r := mux.NewRouter()
-
-	r.HandleFunc("/library", road)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r.HandleFunc("/library", read).Methods(http.MethodGet)
+	r.HandleFunc("/library", create).Methods(http.MethodPost)
+	r.HandleFunc("/library", update).Methods(http.MethodPut)
+	r.HandleFunc("/library", delete).Methods(http.MethodDelete)
+	r.HandleFunc("/library", notFound)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
