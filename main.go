4d9109091118208e8cff2f3061760f68cb12eba3
likejehu <etqash@gmail.com>
2020-02-13 04:17:15 +0200

    added some handling to main

--- main.go
+++ main.go
@@ -7,14 +7,30 @@ import (
 
 type server struct{}
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message": "welcome to library"}`))
+func road(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("get(read) called"))
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("post(create) called"))
+	case "PUT":
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte("put(update) called"))
+	case "DELETE":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("delete called"))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("not found"))
+	}
+
 }
 
 func main() {
-	s := &server{}
-	http.Handle("/library", s)
+
+	http.HandleFunc("/library", road)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
