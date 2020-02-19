cb4662585e206ce0d7f0c093f15dd5bfa15b819b
likejehu <etqash@gmail.com>
2020-02-19 10:56:26 +0200

    rewrote booksDB as type

--- db/db.go
+++ db/db.go
@@ -1,22 +1,12 @@
 package db
 
 import (
-	"fmt"
-
 	"github.com/likejehu/crudapi/models"
-	"github.com/prologic/bitcask"
 )
 
-// BooksDB  is mine books storage representation
-var BooksDB = map[string]*models.Book{}
+type booksDB map[string]*models.Book
 
 // DB is mine representation of database
-var DB, _ = bitcask.Open("/tmp/db")
-
-func main() {
-	DB, _ := bitcask.Open("/tmp/db")
-	defer DB.Close()
-	DB.Put([]byte("Hello"), []byte("World"))
-	val, _ := DB.Get([]byte("Hello"))
-	fmt.Println(val)
+type DB struct {
+	*booksDB
 }
