5540ee1e8c776e3deb2a4285680e41858426c4dc
likejehu <etqash@gmail.com>
2020-02-22 16:50:36 +0200

    put handlers in handlers.go and edited main.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,14 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/go-playground/validator"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo"
+	"github.com/likejehu/crudapi/db"
+	"github.com/likejehu/crudapi/models"
 )
 
 // CustomValidator is mine validator
@@ -12,4 +19,46 @@ type CustomValidator struct {
 // Validate is for  validating
 func (cv *CustomValidator) Validate(i interface{}) error {
 	return cv.validator.Struct(i)
+}
+
+//----------
+// Handlers
+//----------
+
+// UpdateBook is  for  updating books properties
+func UpdateBook(c echo.Context) (err error) {
+	b := new(models.Book)
+	if err := c.Bind(b); err != nil {
+		return err
+	}
+	id := c.Param("id")
+	db.BooksDB[id].Title = b.Title
+	return c.JSON(http.StatusOK, db.BooksDB[id])
+}
+
+// CreateBook is for  creating new book
+func CreateBook(c echo.Context) (err error) {
+
+	b := new(models.Book)
+	if err := c.Bind(b); err != nil {
+		return err
+	}
+	id := uuid.New().String()
+	b.ID = id
+
+	db.Library.Put(id, b)
+	return c.JSON(http.StatusCreated, b)
+}
+
+// GetBook is for  returning book by id
+func GetBook(c echo.Context) (err error) {
+	id := c.Param("id")
+	return c.JSON(http.StatusOK, db.BooksDB[id])
+}
+
+// DeleteBook is for  deleting book by id
+func DeleteBook(c echo.Context) (err error) {
+	id := c.Param("id")
+	delete(db.BooksDB, id)
+	return c.NoContent(http.StatusNoContent)
 }
--- main.go
+++ main.go
@@ -1,52 +1,10 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/google/uuid"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"github.com/likejehu/crudapi/db"
-	"github.com/likejehu/crudapi/models"
+	"github.com/likejehu/crudapi/handlers"
 )
-
-//----------
-// Handlers
-//----------
-
-func updateBook(c echo.Context) (err error) {
-	b := new(models.Book)
-	if err := c.Bind(b); err != nil {
-		return err
-	}
-	id := c.Param("id")
-	db.BooksDB[id].Title = b.Title
-	return c.JSON(http.StatusOK, db.BooksDB[id])
-}
-
-func createBook(c echo.Context) (err error) {
-
-	b := new(models.Book)
-	if err := c.Bind(b); err != nil {
-		return err
-	}
-	id := uuid.New().String()
-	b.ID = id
-
-	db.Library.Put(id, b)
-	return c.JSON(http.StatusCreated, b)
-}
-
-func getBook(c echo.Context) (err error) {
-	id := c.Param("id")
-	return c.JSON(http.StatusOK, db.BooksDB[id])
-}
-
-func deleteBook(c echo.Context) (err error) {
-	id := c.Param("id")
-	delete(db.BooksDB, id)
-	return c.NoContent(http.StatusNoContent)
-}
 
 func main() {
 	e := echo.New()
@@ -56,10 +14,10 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.POST("/books", createBook)
-	e.GET("/books/:id", getBook)
-	e.PUT("/books/:id", updateBook)
-	e.DELETE("/books/:id", deleteBook)
+	e.POST("/books", handlers.CreateBook)
+	e.GET("/books/:id", handlers.GetBook)
+	e.PUT("/books/:id", handlers.UpdateBook)
+	e.DELETE("/books/:id", handlers.DeleteBook)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
