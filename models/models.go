e812eccee36bb101aa78421e8601868d9d33a7d4
likejehu <etqash@gmail.com>
2020-02-24 17:19:32 +0200

    validation now working

--- models/models.go
+++ models/models.go
@@ -8,7 +8,7 @@ type (
 		Author      string `json:"author" form:"author" query:"author" validate:"required"`
 		Publisher   string `json:"publisher" form:"publisher" query:"publisher" validate:"required"`
 		PublishDate string `json:"date" form:"date" query:"date" validate:"required"`
-		Rating      uint8  `json:"rating" form:"rating" query:"rating" validate:"required"`
-		Status      string `json:"status" form:"status" query:"status" validate:"required"`
+		Rating      uint8  `json:"rating" form:"rating" query:"rating" validate:"min=1,max=3"`
+		Status      string `json:"status" form:"status" query:"status" validate:"oneof=CheckedIn CheckedOut"`
 	}
 )
