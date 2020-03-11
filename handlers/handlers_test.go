f63d79e0a2ff9fcd847fa58a1e2de12dc9d0449a
likejehu <etqash@gmail.com>
2020-03-11 17:43:51 +0200

    func TestDeleteBookwithMockery added

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -14,47 +14,6 @@ import (
 	"gopkg.in/go-playground/validator.v10"
 )
 
-type mockingDB struct {
-	B map[string]*models.Book
-}
-
-// Delete is delete func
-func (d *mockingDB) Delete(key string) {
-	delete(d.B, key)
-	return
-}
-
-// Get is read func
-func (d *mockingDB) Get(key string) *models.Book {
-	book := d.B[key]
-	return book
-}
-
-// Post is create func
-func (d *mockingDB) Post(key string, book *models.Book) models.Book {
-	d.B[key] = book
-	return *d.B[key]
-}
-
-// Update is update func
-func (d *mockingDB) Update(key string, book *models.Book) (v *models.Book) {
-	d.B[key] = book
-	v = d.B[key]
-	return
-}
-
-// GetAll is for returning all the books
-func (d *mockingDB) GetAll() map[string]*models.Book {
-	books := d.B
-	return books
-}
-func newmockDB() *mockingDB {
-	var m mockingDB
-	m.B = make(map[string]*models.Book)
-	return &m
-}
-
-var mockDB = newmockDB()
 var bookJSON = `{"title":"SUper kniga","author":"Igor","publisher":"Superizdatel","date":"2020-02-02","rating":3,"status":"CheckedIn"}
 `
 
@@ -83,24 +42,6 @@ func TestCreateBookwithMockery(t *testing.T) {
 	assert.Equal(t, http.StatusCreated, rec.Code)
 	assert.Equal(t, bookJSON, rec.Body.String())
 }
-func TestCreateBook(t *testing.T) {
-	//setup
-	e := echo.New()
-	validator := validator.New()
-	e.Validator = &customValidator{validator}
-	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(bookJSON))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := &Handler{mockDB}
-
-	// Assertions
-	if assert.NoError(t, h.CreateBook(c)) {
-		assert.Equal(t, http.StatusCreated, rec.Code)
-		assert.Equal(t, bookJSON, rec.Body.String())
-	}
-
-}
 
 func TestGetBookwithMockery(t *testing.T) {
 	//setup
@@ -117,27 +58,6 @@ func TestGetBookwithMockery(t *testing.T) {
 	// Assertions
 	assert.Equal(t, http.StatusOK, rec.Code)
 	assert.Equal(t, bookJSON, rec.Body.String())
-}
-func TestGetBook(t *testing.T) {
-	// Setup
-	e := echo.New()
-	e.MaxParam(5)
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	c.SetPath("/books/:id")
-	c.SetParamNames("id")
-	c.SetParamValues("80369cc7-41af-48a0-be9e-a71377bcb337")
-	mockDB.B = map[string]*models.Book{
-		"80369cc7-41af-48a0-be9e-a71377bcb337": &models.Book{"SUper kniga", "Igor", "Superizdatel", "2020-02-02", 3, "CheckedIn"},
-	}
-	h := &Handler{mockDB}
-
-	// Assertions
-	if assert.NoError(t, h.GetBook(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, bookJSON, rec.Body.String())
-	}
 }
 
 func TestDeleteBookwithMockery(t *testing.T) {
@@ -160,25 +80,4 @@ func TestDeleteBookwithMockery(t *testing.T) {
 	assert.Equal(t, http.StatusNoContent, rec.Code)
 	assert.Equal(t, exp, rec.Body.String())
 
-}
-func TestDeleteBook(t *testing.T) {
-	// Setup
-	e := echo.New()
-	e.MaxParam(5)
-	req := httptest.NewRequest(http.MethodDelete, "/", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	c.SetPath("/books/:id")
-	c.SetParamNames("id")
-	c.SetParamValues("80369cc7-41af-48a0-be9e-a71377bcb337")
-	mockDB.B = map[string]*models.Book{
-		"80369cc7-41af-48a0-be9e-a71377bcb337": &models.Book{"SUper kniga", "Igor", "Superizdatel", "2020-02-02", 3, "CheckedIn"},
-	}
-	h := &Handler{mockDB}
-	exp := ""
-	// Assertions
-	if assert.NoError(t, h.DeleteBook(c)) {
-		assert.Equal(t, http.StatusNoContent, rec.Code)
-		assert.Equal(t, exp, rec.Body.String())
-	}
 }
