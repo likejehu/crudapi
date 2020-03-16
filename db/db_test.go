5fc0b53f548e56de27e51ec50a9412bb1f281af4
likejehu <etqash@gmail.com>
2020-03-16 15:33:19 +0200

    rewrote TestNewDB

--- db/db_test.go
+++ db/db_test.go
@@ -8,6 +8,6 @@ func TestNewDB(t *testing.T) {
 	var i interface{} = NewDB()
 	d, ok := i.(*DB)
 	if ok != true {
-		t.Errorf("NewDB() = %v; want type *DB", d)
+		t.Errorf("NewDB() = %T; want type *DB", d)
 	}
 }
