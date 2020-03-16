b637116d9ef6884608162f3a444b8e44098eada9
likejehu <etqash@gmail.com>
2020-03-16 18:21:31 +0200

    added map of books

--- db/db_test.go
+++ db/db_test.go
@@ -2,7 +2,17 @@ package db
 
 import (
 	"testing"
+
+	"github.com/likejehu/crudapi/models"
 )
+
+var testDB = NewDB()
+
+var mb = map[string]*models.Book{
+	"01": &models.Book{Title: "Priklyucheniya Igorya", Author: "Igor", Publisher: "Superizdatel", PublishDate: "2020-02-02", Rating: 0x1, Status: "CheckedIn"},
+	"02": &models.Book{Title: "Novie priklyucheniya Igorya", Author: "Igor", Publisher: "Superizdatel", PublishDate: "2020-02-02", Rating: 0x2, Status: "CheckedIn"},
+	"03": &models.Book{Title: "Supernovie priklyucheniya Igorya", Author: "Igor", Publisher: "Superizdatel", PublishDate: "2020-02-02", Rating: 0x3, Status: "CheckedIn"},
+}
 
 func TestNewDB(t *testing.T) {
 	var i interface{} = NewDB()
@@ -10,4 +20,7 @@ func TestNewDB(t *testing.T) {
 	if ok != true {
 		t.Errorf("NewDB() = %T; want type *DB", d)
 	}
+}
+func TestDelete(t *testing.T) {
+
 }
